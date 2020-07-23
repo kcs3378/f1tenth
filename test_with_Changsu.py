57ae30aa269d9f718bc346792e3559c0295bcb86
chansik0504 <52341609+chansik0504@users.noreply.github.com>
2020-07-23 15:52:51 +0900

    Update test_with_Changsu.py

--- test_with_Changsu.py
+++ test_with_Changsu.py
@@ -1,67 +1,53 @@
 #!/usr/bin/env python
 
 import rospy
-import random
 from ackermann_msgs.msg import AckermannDriveStamped
 from sensor_msgs.msg import LaserScan
-
+import math
 
 ackermann_data = AckermannDriveStamped()
 
 def scan_callback(scan_data):
     scan = scan_data.ranges
-    front = 0
-    left = 0
-    right = 0
 
-    for x in range(825,855):
-        left = left + scan[x]
-    left = left / 30
-    # print("left : ", left)
+    left = scan[840]
+    right = scan[290]
+    front = scan[560]
+    back = scan [10]
 
-    for c in range(275,305):
-        right = right + scan[c]
-    right = right / 30
-    # print("right : ", right)
+    # print("front :", front)
+    # print("right :", right)
+    # print("left :", left)
+    
 
-    for z in range(540,580):
-        front = front + scan[z]
-    front = front / 40
-    # print("front : ", front)
+    if front < 3:
+        if left <= 2 and right >= 2:
+            ackermann_data.drive.steering_angle = -1    
+            ackermann_data.drive.speed = 1
 
-    print("front : ", front)
-    print("right : ", right)
-
-    if front < 1.0:
-        if front < 0.4 and right < 0.4:
-            for i in range(10000):
-                ackermann_data.drive.steering_angle = -1
-                ackermann_data.drive.speed = -0.5
-        if front > 0.4 and right > 0.4:
+        elif right <= 2 and left >= 2:
             ackermann_data.drive.steering_angle = 1
-            ackermann_data.drive.speed = 0.5
-        else:
-            ackermann_data.drive.steering_angle = 5
             ackermann_data.drive.speed = 1
     else:
-        if 0.5 < right < 100:
-            ackermann_data.drive.steering_angle = -1
-            ackermann_data.drive.speed = 0.5
-        elif right < 0.4:
-            ackermann_data.drive.steering_angle = 1
-            ackermann_data.drive.speed = 0.5
-        else:
+        if 1 <= left <= 1.5:
             ackermann_data.drive.steering_angle = 0
-            ackermann_data.drive.speed = 0.5
-    
+            ackermann_data.drive.speed = 1
+        elif left < 1:
+            ackermann_data.drive.steering_angle = -0.05
+            ackermann_data.drive.speed = 1
+        elif left > 1.5:
+            ackermann_data.drive.steering_angle = 0.05
+            ackermann_data.drive.speed = 1
+
+
 def talker():
     rospy.init_node('publisher', anonymous = True)
     rate = rospy.Rate(10)
     rospy.loginfo('start node')
 
     pub = rospy.Publisher('/nav', AckermannDriveStamped, queue_size = 1 )
-    rospy.Subscriber('/scan', LaserScan, scan_callback)
-
+    sub = rospy.Subscriber('/scan', LaserScan, scan_callback)
+    
     while not rospy.is_shutdown():
         # data.header.stamp = rospy.get_time()
         pub.publish(ackermann_data)
@@ -73,9 +59,3 @@ if __name__ == '__main__':
         talker()
     except rospy.ROSInterruptException:
         pass
-
-
-
-
-
-
